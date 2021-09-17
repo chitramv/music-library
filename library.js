7d6a51357770c79a8091efa9f2b2f5bd0c5965ad
Chitra <chitravenkitraman@gmail.com>
2021-09-17 15:49:16 -0600

    added functions as methods

--- library.js
+++ library.js
@@ -20,8 +20,8 @@ const library = {
                       name: "Other Playlist",
                       tracks: ["t03"]
                     }
-             }
-};
+             },
+
 
 /////////////////////////////
 // FUNCTIONS TO IMPLEMENT:
@@ -30,92 +30,92 @@ const library = {
 // prints a list of all playlists, in the form:
 // p01: Coding Music - 2 tracks
 // p02: Other Playlist - 1 tracks
-const printPlaylists = function() {
-  const obj1 = library.playlists;
+printPlaylists : function() {
+  const obj1 = this.playlists;
   for (let elem in obj1){
     console.log(`${elem}: ${obj1[elem]['name']} - ${obj1[elem]['tracks'].length} tracks`);
   }
-};
-//printPlaylists();
+},
+//library.printPlaylists();
 
 
 // prints a list of all tracks, using the following format:
 // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
 // t02: Model View Controller by James Dempsey (WWDC 2003)
 // t03: Four Thirty-Three by John Cage (Woodstock 1952)
-const printTracks = function() {
-  const obj1 = library.tracks;
+printTracks : function() {
+  const obj1 = this.tracks;
   for (let elem in obj1) {
     console.log(`${elem}: ${obj1[elem]['name']} by ${obj1[elem]['artist']} (${obj1[elem]['album']})`);
   }
-}
-//printTracks();
+},
+//library.printTracks();
 // print tracks corresponsing to a trackid
-const printTrackById = function(trackId) {
-       const obj1 = library.tracks;
+printTrackById : function(trackId) {
+       const obj1 = this.tracks;
        const elem = obj1[trackId];
          console.log(`${elem['id']}: ${elem['name']} by ${elem['artist']} (${elem['album']})`);
 
-     }
-    // printTrackById('t01');
+     },
+    //library.printTrackById('t01');
 
 
 // prints a list of tracks for a given playlist, using the following format:
 // p01: Coding Music - 2 tracks
 // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
 // t02: Model View Controller by James Dempsey (WWDC 2003)
-const printPlaylist = function(playlistId) {
-       const obj1 = library.playlists;
+printPlaylist : function(playlistId) {
+       const obj1 = this.playlists;
        const elem = playlistId;
        console.log(`${elem}: ${obj1[elem]['name']} - ${obj1[elem]['tracks'].length} tracks`);
        for (let item of obj1[playlistId]['tracks']){
-              printTrackById(item);
+              this.printTrackById(item);
        }
-}
-//printPlaylist('p01');
+},
+//library.printPlaylist('p01');
 
 // adds an existing track to an existing playlist
-const addTrackToPlaylist = function(trackId, playlistId) {
-       const obj1 = library.playlists;
+addTrackToPlaylist : function(trackId, playlistId) {
+       const obj1 = this.playlists;
        obj2 = obj1[playlistId];
        obj2['tracks'].push(trackId);
        console.log(obj2);
-}
-//addTrackToPlaylist('t01','p02');
+},
+//library.addTrackToPlaylist('t01','p02');
 
 
 // generates a unique id
 // (already implemented: use this for addTrack and addPlaylist)
-const generateUid = function() {
+generateUid : function() {
   return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-}
+},
 
 
 // adds a track to the library
-const addTrack = function(name, artist, album) {
-const obj1 = library.tracks;
-const trackId = generateUid();
+addTrack : function(name, artist, album) {
+const obj1 = this.tracks;
+const trackId = this.generateUid();
 obj1[trackId] = {};
 obj1[trackId]['id'] = trackId;
 obj1[trackId]['name'] = name;
 obj1[trackId]['artist'] = artist;
 obj1[trackId]['album'] = album;
 console.log(obj1[trackId]);
-}
-//addTrack('Closure','Mini','filo');
+},
+//library.addTrack('Closure','Mini','filo');
 
 
 
 // adds a playlist to the library
-const addPlaylist = function(name) {
-  const obj1 = library.playlists;
-  const playlistId = generateUid();
+addPlaylist : function(name) {
+  const obj1 = this.playlists;
+  const playlistId = this.generateUid();
   obj1[playlistId] = {};
   obj1[playlistId]['id'] = playlistId;
   obj1[playlistId]['name'] = name;
   console.log(obj1[playlistId]);
-}
-addPlaylist("Chitra");
+}};
+library.addPlaylist("Chitra");
 
 
 // STRETCH:
